9d0c3db28bf89a00c16a94902612b9357a68e783
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-02-01 09:18:31 -0300

    demo test download from s3

--- server.ts
+++ server.ts
@@ -2,18 +2,46 @@ import express from "express";
 import { readFileSync } from "fs";
 import multer from "multer";
 import UploadToAmazonS3 from "./src/s3";
+import S3 from "aws-sdk/clients/s3";
+
+const s3 = new S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_SECRET_KEY as string,
+  },
+  region: process.env.AWS_BUCKET_REGION,
+});
 
 const app = express();
 
 const upload = multer({ dest: 'uploads/' });
 
-const image = readFileSync('./qrcode.png');
+app.get("/images/:key", async (req, res) => {
+  const { key } = req.params
+
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME as string,
+    Key: key
+  };
+  
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving image from S3');
+    } else {
+      const imageBuffer = data.Body;
+      res.set('Content-Type', 'image/jpeg');
+      res.send(imageBuffer);
+    }
+  });
+})
 
 app.post("/images", upload.single("image"), async (req, res) => {
   const file = req.file!;
-  console.log(file);
+  //console.log(file);
   const result = await UploadToAmazonS3.upload(file);
-  console.log(result);
+  //console.log(result);
+
   res.send("ok");
 })
 
--- src/s3.ts
+++ src/s3.ts
@@ -8,24 +8,35 @@ const s3 = new S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY as string,
     secretAccessKey: process.env.AWS_SECRET_KEY as string,
-  }, region: process.env.AWS_BUCKET_REGION,
+  },
+  region: process.env.AWS_BUCKET_REGION,
 });
 
 // uploads to s3
 class UploadToAmazonS3 {
   public upload(file: Express.Multer.File): Promise<S3.ManagedUpload.SendData> {
-    const fileStream = fs.createReadStream(file.path)
+    const fileStream = fs.createReadStream(file.path);
 
     const uploadParams = {
       Bucket: process.env.AWS_BUCKET_NAME as string,
       Body: fileStream,
-      Key: file.filename
-    }
+      Key: file.filename,
+    };
 
     return s3.upload(uploadParams).promise();
   }
+
+  public async download(fileKey: string) {
+    const downloadParams = {
+      Key: fileKey,
+      Bucket: process.env.AWS_BUCKET_NAME!,
+    };
+
+    s3.getObject(downloadParams, (err, data) => {
+        return data.Body;
+    })
+  }
 }
 
-export default new UploadToAmazonS3;
-// downloads to s3
-
+export default new UploadToAmazonS3();
+// downloads from s3
